1fbf13f7dde58b6653785b274da3d949a3cfd87d
MdSauravChowdhury <sauravchowdhury165@gmail.com>
2019-01-24 00:26:12 +0600

    This is our first commit this is our landing page view

---
+++ js/active.js
@@ -0,0 +1,26 @@
+(function ($) {
+    "use strict";
+    
+    jQuery(Document).ready(function($){
+    
+    $(".slide-wrapper").oulCarousel({
+        item: 1,
+        nav: true,
+        dots: false,
+        autoplay: false,
+        loop: true,
+    });
+                            
+                            
+});
+    
+
+jQuery(window).load(function(){
+    
+    
+    
+    
+}); 
+
+
+}(jQuery)); |
